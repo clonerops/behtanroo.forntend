36f88a0d9ba896bf3a509fc664dc30aaad376ba2
Cloner ops <clonerops@gmail.com>
2024-01-30 15:31:20 +0330

    axios config

--- src/_cloner/helpers/axiosConfig.ts
+++ src/_cloner/helpers/axiosConfig.ts
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 import { getRefreshToken } from "./reusableFunction";
 
 export const dashboardHttp = axios.create({
-    baseURL: "https://localhost:5000/api/",
+    baseURL: "https://behtanroo.ir/api/",
     headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${Cookies.get("token")}`,
@@ -11,7 +11,7 @@ export const dashboardHttp = axios.create({
 });
 
 export const http = axios.create({
-    baseURL: "http://localhost:5000/api/",
+    baseURL: "https://behtanroo.ir/api/",
     headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${Cookies.get("token")}`,
