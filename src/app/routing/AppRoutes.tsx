86cdbb5d97b8fa5184ec93b303369c360f167736
Cloner ops <clonerops@gmail.com>
2024-02-05 11:11:22 +0330

    اشساقخعفثق

--- src/app/routing/AppRoutes.tsx
+++ src/app/routing/AppRoutes.tsx
@@ -6,7 +6,7 @@
  */
 
 import { FC } from "react";
-import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate, HashRouter } from "react-router-dom";
 import { PrivateRoutes } from "./PrivateRoutes";
 import { ErrorsPage } from "../modules/errors/ErrorsPage";
 import { App } from "../App";
@@ -18,7 +18,7 @@ const { PUBLIC_URL } = process.env;
 
 const AppRoutes: FC = () => {
     return (
-        <BrowserRouter basename={PUBLIC_URL}>
+        <HashRouter basename={PUBLIC_URL}>
             <Routes>
                 <Route element={<App />}>
                     <Route path="error/*" element={<ErrorsPage />} />
@@ -38,7 +38,7 @@ const AppRoutes: FC = () => {
                     )}
                 </Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
